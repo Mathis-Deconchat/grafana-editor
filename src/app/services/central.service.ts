e3d49cc71a9b3773e6e97c64a47eaef0d232bf5e
mathis.deconchat@gmail.com <mathis.deconchat@gmail.com>
2021-05-03 12:33:57 +0000

    not wprking at all

--- src/app/services/central.service.ts
+++ src/app/services/central.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { DashboardFetcherService } from './dashboard-fetcher.service'
 import { Dashboard } from '../dashboard';
 import { Panels, DashboardTitles } from '../panels'
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, EMPTY } from 'rxjs';
 import { DashboardSearch } from '../dashboard-search';
 
 
@@ -11,32 +11,98 @@ import { DashboardSearch } from '../dashboard-search';
 })
 export class CentralService {
 
-
-  private _arrayOfTitlesSource = new BehaviorSubject<DashboardTitles[]>([{ 'title': '', 'uid': '' }, { 'title': '', 'uid': '' }])
-  arrayOfTitles = this._arrayOfTitlesSource.asObservable()
-
-
-  constructor(
-    private _dashboardFetcherService: DashboardFetcherService,
-
-  ) { }
-  _dashboard?: Dashboard
   _dashboardList?: DashboardSearch[]
-  _panelsTitles?: DashboardTitles[]
+  private _rxDashboardList = new BehaviorSubject<DashboardSearch[]>([])
 
-  loadInitialList() {
+  _selectedDashboard: string = ''
+  private _rxSelectedDashboard = new BehaviorSubject<string>('')
+
+  _dashboard?: Dashboard
+  private _rxDashboardContent = new BehaviorSubject<any>('')
+
+  constructor(private _dashboardFetcherService: DashboardFetcherService,) {
+    this._selectedDashboard
+    this.loadInitialTitles()
+
+    this._rxSelectedDashboard.next(this._selectedDashboard)
+    this
+
+
+  }
+
+
+
+  _panelsTitles?: Panels[]
+
+
+
+  get titles() {
+    return this._rxDashboardList.asObservable()
+  }
+
+  get selectedDashboard() {
+    return this._rxSelectedDashboard.asObservable()
+  }
+
+  get dashboard() {
+    return this._rxDashboardContent.asObservable()
+  }
+
+
+
+
+  loadInitialTitles() {
     this._dashboardFetcherService.getAllDashboard()
+      .subscribe(response => {
+        let filteredResponse = response.filter(response => {
+          return response.type = 'dash-db'
+        })
+        this._rxDashboardList.next(filteredResponse);
+      })
+  }
+
+  updateSelectedDashboard(uid: string) {
+    this._rxDashboardContent.next('')
+    this._rxSelectedDashboard.next('')
+
+    this._rxSelectedDashboard.next(uid)
+    this._dashboardFetcherService.getDashboard(uid)
       .subscribe(
         res => {
-          let filteredResponse = res.filter(res => {
-            return res.type = 'dash-db'
-          })
-          return filteredResponse;
+          this._rxDashboardContent.next(res)
         },
         err => {
           console.log(err)
-        })
+        }
+      )
   }
+
+
+
+
+
+
+
+
+  // private _arrayOfTitlesSource = new BehaviorSubject<Dashboard>([{ 'title': '', 'uid': '' }, { 'title': '', 'uid': '' }])
+  // arrayOfTitles = this._arrayOfTitlesSource.asObservable()
+
+
+
+
+  // loadInitialList() {
+  //   this._dashboardFetcherService.getAllDashboard()
+  //     .subscribe(
+  //       res => {
+  //         let filteredResponse = res.filter(res => {
+  //           return res.type = 'dash-db'
+  //         })
+  //         return filteredResponse;
+  //       },
+  //       err => {
+  //         console.log(err)
+  //       })
+  // }
 
   // getList(dashboardTitles: DashboardTitles[]) {
   //   this._arrayOfTitlesSource.next(dashboardTitles)
